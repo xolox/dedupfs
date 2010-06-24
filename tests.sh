9ec03b17d09d92a6a725dd9180714ef0baf20eb0
Peter Odding <peter@peterodding.com>
2010-06-24 19:04:38 +0200

    Added more automated tests
     * Made the "write arbitrary data" tests use smaller
       files to make the test script faster.
     * Added test numbers to text folds.
     * Verify that written data persists when remounted.
     * Verify that garbage collection of unused data blocks works.
     * Verify that garbage collection of interned path segments works.

--- tests.sh
+++ tests.sh
@@ -5,14 +5,13 @@ ROOTDIR="/tmp/dedupfs-tests-$TIMESTAMP"
 MOUNTPOINT="$ROOTDIR/mountpoint"
 METASTORE="$ROOTDIR/metastore.sqlite3"
 DATASTORE="$ROOTDIR/datastore.db"
-WAITTIME=2
+WAITTIME=1
 TESTNO=1
 
 # Initialization. {{{1
 
 FAIL () {
   FAIL_INTERNAL "$@"
-  CLEANUP
   exit 1
 }
 
@@ -29,19 +28,10 @@ FAIL_INTERNAL () {
 }
 
 CLEANUP () {
-
-  sleep $WAITTIME
-
-  if ! fusermount -u "$MOUNTPOINT"; then
-    FAIL_INTERNAL "$0:$LINENO: Failed to unmount the mount point?!"
-  fi
-
-  sleep $WAITTIME
-
+  DO_UNMOUNT
   if ! rm -R "$ROOTDIR"; then
     FAIL_INTERNAL "$0:$LINENO: Failed to delete temporary directory!"
   fi
-
 }
 
 # Create the root and mount directories.
@@ -51,19 +41,33 @@ if [ ! -d "$MOUNTPOINT" ]; then
   exit 1
 fi
 
-# Mount the file system using the two temporary databases.
-OPTS="--verify-writes --compress=lzo --metastore=$METASTORE --datastore=$DATASTORE"
-if [ "$1" = "-d" ]; then
-  python dedupfs.py -fvv $OPTS "$MOUNTPOINT" &
-else
-  python dedupfs.py $OPTS "$MOUNTPOINT"
-fi
+DO_MOUNT () {
+  # Mount the file system using the two temporary databases.
+  python dedupfs.py -fv "$@" "--metastore=$METASTORE" "--datastore=$DATASTORE" "$MOUNTPOINT" &
+  # Wait a while before accessing the mount point, to
+  # make sure the file system has been fully initialized.
+  while true; do
+    sleep $WAITTIME
+    if mount | grep -q "$MOUNTPOINT"; then break; fi
+  done
+}
 
-# Wait a while before accessing the mount point, to
-# make sure the file system has been fully initialized.
-sleep $WAITTIME
+DO_UNMOUNT () {
+  if mount | grep -q "$MOUNTPOINT"; then
+    sleep $WAITTIME
+    if ! fusermount -u "$MOUNTPOINT"; then
+      FAIL_INTERNAL "$0:$LINENO: Failed to unmount the mount point?!"
+    fi
+    while true; do
+      sleep $WAITTIME
+      if ! mount | grep -q "$MOUNTPOINT"; then break; fi
+    done
+  fi
+}
 
-# Test hard link counts with mkdir(), rmdir() and rename(). {{{1
+DO_MOUNT --verify-writes --compress=lzo
+
+# Tests 1-8: Test hard link counts with mkdir(), rmdir() and rename(). {{{1
 
 CHECK_NLINK () {
   NLINK=`ls -ld "$1" | awk '{print $2}'`
@@ -74,13 +78,13 @@ FEEDBACK () {
   MESSAGE "Running test $1"
 }
 
-# Check link count of file system root. {{{2
+# Test 1: Check link count of file system root. {{{2
 
 FEEDBACK $TESTNO
 TESTNO=$[$TESTNO + 1]
 CHECK_NLINK "$MOUNTPOINT" 2 $LINENO
 
-# Check link count of newly created file. {{{2
+# Test 2: Check link count of newly created file. {{{2
 
 FEEDBACK $TESTNO
 TESTNO=$[$TESTNO + 1]
@@ -89,7 +93,7 @@ touch "$FILE"
 CHECK_NLINK "$FILE" 1 $LINENO
 CHECK_NLINK "$MOUNTPOINT" 2 $LINENO
 
-# Check link count of hard link to existing file. {{{2
+# Test 3: Check link count of hard link to existing file. {{{2
 
 FEEDBACK $TESTNO
 TESTNO=$[$TESTNO + 1]
@@ -103,7 +107,7 @@ unlink "$LINK"
 CHECK_NLINK "$FILE" 2 $LINENO
 CHECK_NLINK "$MOUNTPOINT" 2 $LINENO
 
-# Check link count of newly created subdirectory. {{{2
+# Test 4: Check link count of newly created subdirectory. {{{2
 
 FEEDBACK $TESTNO
 TESTNO=$[$TESTNO + 1]
@@ -116,14 +120,14 @@ fi
 
 CHECK_NLINK "$SUBDIR" 2 $LINENO
 
-# Check that nlink of root is incremented by one (because of subdirectory created above). {{{2
+# Test 5: Check that nlink of root is incremented by one (because of subdirectory created above). {{{2
 
 FEEDBACK $TESTNO
 TESTNO=$[$TESTNO + 1]
 
 CHECK_NLINK "$MOUNTPOINT" 3 $LINENO
 
-# Check that non-empty directories cannot be removed with rmdir(). {{{2
+# Test 6: Check that non-empty directories cannot be removed with rmdir(). {{{2
 
 FEEDBACK $TESTNO
 TESTNO=$[$TESTNO + 1]
@@ -136,14 +140,14 @@ elif ! rm -R "$SUBDIR"; then
   FAIL "$0:$LINENO: Failed to recursively delete directory?!"
 fi
 
-# Check that link count of root is decremented by one (because of subdirectory deleted above). {{{2
+# Test 7: Check that link count of root is decremented by one (because of subdirectory deleted above). {{{2
 
 FEEDBACK $TESTNO
 TESTNO=$[$TESTNO + 1]
 
 CHECK_NLINK "$MOUNTPOINT" 2 $LINENO
 
-# Check that link counts are updated when directories are renamed. {{{2
+# Test 8: Check that link counts are updated when directories are renamed. {{{2
 
 FEEDBACK $TESTNO
 TESTNO=$[$TESTNO + 1]
@@ -156,7 +160,7 @@ mv -T "$ORIGDIR/subdir" "$REPLDIR/subdir"
 CHECK_NLINK "$ORIGDIR" 2 $LINENO
 CHECK_NLINK "$REPLDIR" 3 $LINENO
 
-# Write random binary data to file system and verify that it reads back unchanged. {{{1
+# Tests 9-14: Write random binary data to file system and verify that it reads back unchanged. {{{1
 
 TESTDATA="$ROOTDIR/testdata"
 
@@ -164,25 +168,73 @@ WRITE_TESTNO=0
 while [ $WRITE_TESTNO -le 5 ]; do
   FEEDBACK $TESTNO
   TESTNO=$[$TESTNO + 1]
-
-  NBYTES=$[$RANDOM % (1024 * 512)]
+  NBYTES=$[$RANDOM % (1024 * 257)]
   head -c $NBYTES /dev/urandom > "$TESTDATA"
   WRITE_FILE="$MOUNTPOINT/$RANDOM"
   cp -a "$TESTDATA" "$WRITE_FILE"
   sleep $WAITTIME
   if ! cmp -s "$TESTDATA" "$WRITE_FILE"; then
-    FAIL "Failed to verify $WRITE_FILE of $NBYTES bytes!"
-    echo "Differences:"
-    ls -l "$TESTDATA" "$WRITE_FILE"
-    cmp -lb "$TESTDATA" "$WRITE_FILE"
-    break
+    (sleep 1
+     echo "Differences:"
+     ls -l "$TESTDATA" "$WRITE_FILE"
+     cmp -lb "$TESTDATA" "$WRITE_FILE") &
+    FAIL "$0:$LINENO: Failed to verify $WRITE_FILE of $NBYTES bytes!"
   fi
-
-  rm "$WRITE_FILE" "$TESTDATA"
   WRITE_TESTNO=$[$WRITE_TESTNO + 1]
 done
 
-# Cleanup. {{{1
+# Test 15: Verify that written data persists when remounted. {{{1
+
+FEEDBACK $TESTNO
+TESTNO=$[$TESTNO + 1]
+
+DO_UNMOUNT
+DO_MOUNT --nogc # <- important for the following tests.
+if ! cmp -s "$TESTDATA" "$WRITE_FILE"; then
+  (sleep 1
+   echo "Differences:"
+   ls -l "$TESTDATA" "$WRITE_FILE"
+   cmp -lb "$TESTDATA" "$WRITE_FILE") &
+  FAIL "$0:$LINENO: Failed to verify $WRITE_FILE of $NBYTES bytes!"
+fi
+
+# Test 16: Verify that garbage collection of unused data blocks works. {{{1
+
+FEEDBACK $TESTNO
+TESTNO=$[$TESTNO + 1]
+
+DO_UNMOUNT
+FULL_SIZE=`ls -l "$DATASTORE" | awk '{print $5}'`
+HALF_SIZE=$[$FULL_SIZE / 2]
+DO_MOUNT
+rm $MOUNTPOINT/* 2>/dev/null
+DO_UNMOUNT
+REDUCED_SIZE=`ls -l "$DATASTORE" | awk '{print $5}'`
+[ $REDUCED_SIZE -lt $HALF_SIZE ] || FAIL "$0:$LINENO: Failed to verify effectiveness of data block garbage collection! (Full size of data store: $FULL_SIZE, reduced size: $REDUCED_SIZE)"
+
+# Test 17: Verify that garbage collection of interned path segments works. {{{1
+
+DO_MOUNT --nosync
+SEGMENTGCDIR="$MOUNTPOINT/gc-of-segments-test"
+mkdir "$SEGMENTGCDIR"
+for ((i=0;i<=512;i+=1)); do
+  echo -ne "\rCreating segment $i"
+  touch "$SEGMENTGCDIR/$i"
+done
+echo -ne "\rSyncing to disk using unmount"
+DO_UNMOUNT
+FULL_SIZE=`ls -l "$METASTORE" | awk '{print $5}'`
+HALF_SIZE=$[$FULL_SIZE / 2]
+echo -ne "\rDeleting segments"
+DO_MOUNT --nosync
+rm -R "$SEGMENTGCDIR"
+echo -ne "\rSyncing to disk using unmount"
+DO_UNMOUNT
+REDUCED_SIZE=`ls -l "$METASTORE" | awk '{print $5}'`
+[ $REDUCED_SIZE -lt $HALF_SIZE ] || FAIL "$0:$LINENO: Failed to verify effectiveness of interned string garbage collection! (Full size of metadata store: $FULL_SIZE, reduced size: $REDUCED_SIZE)"
+echo -ne "\r"
+
+# Finalization. {{{1
 
 CLEANUP
 MESSAGE "All tests passed!"
